df9580f2d0578f897fff4a0d68a2e2c26443541f
Gabe Weymouth <gweymouth@tudelft.nl>
2023-05-19 10:39:01 -0600

    Hack-a-thon update

---
+++ src/NACA_sdf.jl
@@ -0,0 +1,65 @@
+using Plots,StaticArrays,ForwardDiff
+"""
+    parametric_sdf(curve)
+
+Create a function to find the signed distance from a point `X` in 2D space
+to a parametric `curve(t)` where `t ∈ [0,1]`. The resulting function uses 
+ForwardDiff and a simple multi-start Newton root-finding method to identifiy 
+`t` closest to `X`. The returned function runs on GPU and has optional arguments:
+
+    function(X,tol=0.01,t₀=(0.01,0.5,0.99),itmx=5)
+
+where `tol` is the tolerance in `t`, `t₀` are the multi-start points (along 
+with the bounds 0,1), and `itmx` is the max iterations. For example:
+
+    R = 2
+    circle_SDF = parametric_sdf(t->SA[R*sin(2π*t),R*cos(2π*t)])
+    @assert circle_SDF(SA[3,4],tol=1e-8) ≈ 5-R
+
+Note that sign of the SDF depends on the direction of `curve`. The example above
+wraps clockwise such that distances outside the circle are positive.
+"""
+function parametric_sdf(curve)
+    dcurve(t) = ForwardDiff.derivative(curve,t)
+    ddcurve(t) = ForwardDiff.derivative(dcurve,t)
+    norm(t) = (s=dcurve(t); SA[-s[2],s[1]])
+    vector(X,t) = X-curve(t)
+    align(X,t) = vector(X,t)'*dcurve(t)
+    dalign(X,t) = vector(X,t)'*ddcurve(t)-sum(abs2,dcurve(t))
+    distance(X,t) = (v=vector(X,t); copysign(√(v'*v),norm(t)'*v))
+    function(X,tol=0.01,t₀=(0.01,0.5,0.99),itmx=5)
+        # distance to ends
+        dmin = distance(X,0.)
+        d = distance(X,1.)
+        abs(d)<abs(dmin) && (dmin=d)
+        # check for smaller distnace along curve
+        for t in t₀
+            for _ in 1:itmx # Newton root finding
+                dt = align(X,t)/dalign(X,t)
+                t -= dt
+                t = clamp(t,0.,1.)
+                (abs(dt)<tol || t==0 || t==1) && break
+            end
+            d = distance(X,t)
+            abs(d)<abs(dmin) && (dmin=d)
+        end
+        dmin
+    end
+end
+
+curve(t::T,thk) where T = SA[t^2,T(5thk*(0.2969t-0.126t^2-0.3516t^4+0.2843t^6-0.1036t^8))]
+NACA_sdf(thk=0.12) = parametric_sdf(t->curve(t,thk))
+
+NACA_sdf()(SA[-0.5,0])
+NACA_sdf()(SA[1.5,0])
+NACA_sdf()(SA[.3,0.06+0.5])
+
+function test()
+    sdf(x,y) = NACA_sdf()(SA[x,y])
+    x = range(-0.5, 2.5, length=100)
+    y = range(0, 0.5, length=50)
+    z = sdf.(x',y)
+    contour(x,y,z,aspect_ratio=:equal, legend=false, border=:none)
+    contour!(x,y,z,aspect_ratio=:equal, legend=false, border=:none,levels=[0],color=:green)
+    savefig(".\\figures\\distance function.png")
+end
--- src/simulation.jl
+++ src/simulation.jl
@@ -1,37 +1,59 @@
-using WaterLily,Plots,StaticArrays
-include("bezier_sdf.jl")
-export bezier_sdf
+using WaterLily,Plots,StaticArrays,CUDA
+include("NACA_sdf.jl")
+export NACA_sdf
 
-function get_omega!(sim)
-    body(I) = sum(WaterLily.ϕ(i,CartesianIndex(I,i),sim.flow.μ₀) for i ∈ 1:2)/2
-    @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u) * body(I) * sim.L / sim.U
+function location(t,t₁=0.1,t₂=0.6,U=1) 
+    if t<t₁        # ramp-up
+        0.5U/t₁*t^2
+    elseif t<t₂    # steady velocity
+        U*(t-0.5t₁)
+    elseif t<t₂+t₁ # ramp-down
+        U*(t-0.5t₁)-0.5U/t₁*(t-t₂)^2
+    else           # stop
+        U*t₂
+    end
+end
+
+function foil(;L=32,α=7π/180,t₁=0.1,t₂=1,thk=0.1,Re=5e3,U=1,n=4,m=3,mem=Array)
+    # Define (unsteady) pose of foil
+    center = L*SA[2,m-1]
+    map(x,t) = SA[cos(α) sin(α); -sin(α) cos(α)]*(x-center-L*SA[location(t/L,t₁,t₂),0])
+
+    # Define sdf to symmetric and deflected foil
+    sdf(x,t) = L*NACA_sdf(thk)(SA[-x[1],abs(x[2])]/L)
+
+    Simulation((n*L,m*L),(0,0),L;U,ν=U*L/Re,body=AutoBody(sdf,map),mem)
+end
+
+begin # create sim and check foil pose
+    @assert CUDA.functional()
+    sim = foil(L=2^7,mem=CUDA.CuArray,α=10π/180);
+    dat = sim.flow.σ[inside(sim.flow.σ)] |> Array;
+    @gif for t in 0:0.05:1.5
+        measure!(sim,t*sim.L)
+        @show t,maximum(sim.flow.V)
+        copyto!(dat, sim.flow.σ[inside(sim.flow.σ)]) # copy from GPU
+        contour(dat', dpi=300, levels=-1:1, apect_ratio=:equal, legend=false)
+    end
+end
+
+function WaterLily.CFL(a::Flow)
+    @inside a.σ[I] = WaterLily.flux_out(I,a.u)
+    min(4.,inv(maximum(a.σ)+5a.ν)) # keep ΔtU/L≤4
+end
+
+function get_omega!(dat,sim)
+    @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u) * sim.L / sim.U
+    copyto!(dat,sim.flow.σ[inside(sim.flow.σ)])
 end
 
 plot_vorticity(ω; limit=maximum(abs,ω)) =contourf(ω',dpi=300,
     color=palette(:BuGn), clims=(-limit, limit), linewidth=0,
     aspect_ratio=:equal, legend=false, border=:none)
 
-function foil(L;d=0,Re=38e3,U=1,n=6,m=3)   
-    # Pitching motion around the pivot
-    ω = 2π*U/L # reduced frequency k=π
-    center = SA[1,m/2] # foil placement in domain
-    pivot = SA[.1,0] # pitch location from center
-    function map(x,t)
-        α = 6π/180*cos(ω*t)
-        SA[cos(α) sin(α); -sin(α) cos(α)]*(x/L-center-pivot) + pivot
-    end
-
-    # Define sdf to symmetric and deflected foil
-    symmetric = bezier_sdf(SA[0,0],SA[0,0.1],SA[0.5,0.12],SA[1.,0.])
-    deflect(x) = max(0,x-0.3)^2/0.7^2
-    sdf(x,time) = L*symmetric(SA[x[1],abs(x[2]+d*deflect(x[1]))])
-
-    Simulation((n*L+2,m*L+2),[U,0],L;ν=U*L/Re,body=AutoBody(sdf,map))
-end
-
-function test()
-    sim = foil(32,d=0);
-    sim_step!(sim,15);
-    get_omega!(sim);
-    plot_vorticity(sim.flow.σ, limit=10)
+@gif for t in range(0,6,100) # make a video
+    sim_step!(sim,t)
+    get_omega!(dat,sim)
+    plot_vorticity(dat,limit=4)
+    @show t
 end
